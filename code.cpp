f2e8ee63bfeb38790107720416e77c908c6182ba
R. Manasvi <92326435+RManasvi@users.noreply.github.com>
2026-06-09 18:53:09 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  // Include the input-output stream library for using cout
+using namespace std; // Use the standard namespace to avoid typing 'std::' before every standard function
+
+int main()
+{                      // Main function where the program starts executing
+    cout << "R" "\nManasvi" << endl; // Output: R Manasvi
+    
+    ; // Output "hello" to the console
+    return 0;          // Exit the program with a success status
+}
+//g++ -o code code.cpp------> compile a file
+//.\code.exe-----> run exe file
